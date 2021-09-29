e8755a97d8d819baf1797bb5d0e4e117f7b13759
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-09-29 14:52:42 +0200

    Add changes to create to fix error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   resources :transactions
   resources :measurements
+=======
+  resources :measurements, only: [:index, :show, :create]
+  get 'measurements/:id' => 'measurements#show'
+
+>>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
