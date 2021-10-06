1b3073e6b1d8a483448ad01da59f6e3ef94682b6
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-10-06 15:16:43 +0200

    Change origin url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://guarded-brushlands-05784.herokuapp.com"
+    origins "https://track-it-finance.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
